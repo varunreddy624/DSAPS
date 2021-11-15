b927ed555ad09c6bad4a4922c654141e3166382c
varunreddy04022000 <varunreddy04022000@gmail.com>
2021-11-15 18:08:14 +0530

    assign4 q3done;q2 almost done

--- 2/main.cpp
+++ 2/main.cpp
@@ -10,6 +10,14 @@
 
 using namespace std;
 
+typedef struct fileData{
+    int data;
+    int valuesOver;
+    int totalValuesPossible;
+    FILE* fileptr;
+}fileData;
+
+
 int main(int argc,char** argv){
     FILE *filePointer;
     filePointer = fopen(argv[1], "r");
@@ -39,6 +47,7 @@ int main(int argc,char** argv){
     int numberOfFiles = c / NUMBER_OF_INTS_PER_FILE;
 
     vector<int> fileArray;
+    vector<fileData> fileMetaData;
 
     for(int i=0;i<numberOfFiles;i++){
 
@@ -52,12 +61,14 @@ int main(int argc,char** argv){
         }
         sort(fileArray.begin(),fileArray.end());
 
-        FILE *filePointertemp=fopen(("temp_files/temp"+to_string(i)+".txt").c_str(), "w");
+        FILE *filePointerTemp=fopen(("temp_files/"+to_string(i)+".txt").c_str(), "w");
+
+        fileData d = {fileArray[0],0,NUMBER_OF_INTS_PER_FILE,filePointerTemp};
+        fileMetaData.push_back(d);
+
         t= 0;
         while(t<NUMBER_OF_INTS_PER_FILE)
-            fprintf(filePointertemp,"%d\n",fileArray[t++]);   
-
-        fclose(filePointertemp);
+            fprintf(filePointerTemp,"%d,",fileArray[t++]);   
     }
 
     if(c%NUMBER_OF_INTS_PER_FILE != 0){
@@ -73,14 +84,17 @@ int main(int argc,char** argv){
         }
         sort(fileArray.begin(),fileArray.end());
 
-        FILE *filePointertemp=fopen(("temp_files/temp"+to_string(numberOfFiles-1)+".txt").c_str(), "w");
+        FILE *filePointerTemp=fopen(("temp_files/"+to_string(numberOfFiles-1)+".txt").c_str(), "w");
+
+        fileData d = {fileArray[0],0,noOfIntsInLastFile,filePointerTemp};
+        fileMetaData.push_back(d);
+
         t= 0;
         while(t<noOfIntsInLastFile)
-            fprintf(filePointertemp,"%d\n",fileArray[t++]);   
-
-        fclose(filePointertemp);
+            fprintf(filePointerTemp,"%d,",fileArray[t++]);   
     }
     fclose(filePointer);
 
     // --------------------------------------------
+    
 }
